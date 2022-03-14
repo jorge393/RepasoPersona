5c32bd0d7f5daa2c33ca4877b688faa006d598a0
luchoxx87 <jorgisoyo@gmai.com>
2022-03-14 17:04:49 -0300

    update test

--- RepasoPersona.Test/PersonaTest.cs
+++ RepasoPersona.Test/PersonaTest.cs
@@ -7,14 +7,20 @@ namespace RepasoPersona.Test
     public class PersonaTest
     {
         public Persona Pepito {get;set;}
-        
+        public PersonaTest () => Pepito = new Persona("Pepito","Gomez",0);
         [Fact]
         public void Constructor()
         {
-            Pepito = new Persona("Pepito","Gomez",0);
             Assert.Equal("Pepito",Pepito.Nombre);
             Assert.Equal("Gomez",Pepito.Apellido);
             Assert.Equal(0,Pepito.Efectivo);
+        }
+        [Fact]
+        public void AcreditarPositivo()
+        {
+            double esperado = 1000;
+            Pepito.Acreditar(1000);
+            Assert.Equal(esperado,Pepito.Efectivo,3);
         }
     }
 }
